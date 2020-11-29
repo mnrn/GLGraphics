03331bab99e44dc0fecd351d44ef59e8707307cc
ayase <paper.airplane01@gmail.com>
2020-11-29 21:05:16 +0900

    大文字小文字の区別がうまく行っていなかった問題を解決しました。

--- common/Primitive/Cube.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Primitive/TriangleMesh.h"
-
-class Cube : public TriangleMesh {
-public:
-  Cube(GLfloat size = 1.0f);
-};
--- common/Primitive/Drawable.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class Drawable {
-public:
-  virtual ~Drawable() = default;
-  virtual void Render() const = 0;
-};
