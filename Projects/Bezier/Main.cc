28f6b1666da9bb372032c3e4e0b636ea133d3fa6
ayase <paper.airplane01@gmail.com>
2020-12-05 01:28:56 +0900

    readmeに制作環境とビルドの項目を追加しました。

--- Projects/Bezier/Main.cc
+++ Projects/Bezier/Main.cc
@@ -10,6 +10,7 @@
 
 #include "App.h"
 #include "SceneBezier.h"
+#include "SceneLoop.h"
 
 // ********************************************************************************
 // Entry point
@@ -22,18 +23,11 @@ int main(
     int argc, char **argv
 #endif
 ) {
-  App app("Hello Bezier!!!");
+  App app("Bezier Curve Tesselation");
 
   // Create scene
   std::unique_ptr<Scene> scene = std::make_unique<SceneBezier>();
 
   // Enter the main loop
-  return app.Run(
-      [&scene](int w, int h) {
-        scene->SetDimensions(w, h);
-        scene->OnInit();
-        scene->OnResize(w, h);
-      },
-      [&scene](float dt) { scene->OnUpdate(dt); },
-      [&scene]() { scene->OnRender(); });
+  return SceneLoop::Run(app, std::move(scene));
 }
